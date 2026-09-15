8914ba45a996d556d45cb2856815c21d325924f8
TZGaming <tomzuidervaart@gmail.com>
2026-09-16 00:16:26 +0200

    #2 #5 Basis popup en button styling toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,15 @@
     const person = data.person;
 </script>
 
-<article>
-    <h1>{ person.name }</h1>
-    <p>{@html person.bio }</p>
-</article>
+<main>
+    <article>
+        <h1>{ person.name }</h1>
+        <p>{@html person.bio }</p>
+    </article>
+
+    <div class="info-buttons">
+        <button>Knop 1</button>
+        <button>Knop 2</button>
+        <button>Knop 3</button>
+    </div>
+</main>
