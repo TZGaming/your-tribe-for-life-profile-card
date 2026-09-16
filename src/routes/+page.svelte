f9af6478fef227c5da8e5020af4134e3c47a9c56
TZGaming <tomzuidervaart@gmail.com>
2026-09-17 00:19:28 +0200

    #4 #2 Mugshot foto ingeladen en toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script>
     let { data } = $props();
-    const person = data.person;
+    const person = $derived(data.person);
 </script>
 
 <main>
     <article>
-        <h1>{ person.name }</h1>
-        <p>{@html person.bio }</p>
+        <div class="general-info-split">
+            <div class="info-text">
+                <h1>{ person.name }</h1>
+                <p>{@html person.bio }</p>
+            </div>
+            <div class="info-mugshot">
+                <img src="https://fdnd.directus.app/assets/{person.mugshot}?height=200" alt="" width="200" height="200"/>
+            </div>
+        </div>
     </article>
 
     <div class="info-buttons">
